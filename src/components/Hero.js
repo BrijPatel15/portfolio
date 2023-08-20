b10df14be512f7250d5f299a316f500c2c686490
Brij Patel <brij.15@live.ca>
2023-08-20 13:39:11 -0400

    More cleanup

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 const HeroSection = styled.section`
